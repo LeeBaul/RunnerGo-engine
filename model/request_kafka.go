be8ff6e136769b28735665cffac45073de21b1d5
guosongsong <guosongsong@apipost.cn>
2022-10-25 22:11:12 +0800

    添加partition分区标识

--- model/request_kafka.go
+++ model/request_kafka.go
@@ -27,7 +27,6 @@ func SendKafkaMsg(kafkaProducer sarama.SyncProducer, resultDataMsgCh chan *Resul
 			DataMsg := &sarama.ProducerMessage{}
 			DataMsg.Topic = topic
 			DataMsg.Partition = partition
-			DataMsg.Key = sarama.StringEncoder(resultDataMsg.ReportId)
 			DataMsg.Value = sarama.StringEncoder(msg)
 			_, _, err = kafkaProducer.SendMessage(DataMsg)
 			if err != nil {
@@ -35,27 +34,27 @@ func SendKafkaMsg(kafkaProducer sarama.SyncProducer, resultDataMsgCh chan *Resul
 				break
 			}
 		} else {
-			//// 发送结束消息
-			//result := new(ResultDataMsg)
-			//result.ReportId = reportId
-			//result.End = true
-			//result.MachineNum = num
-			//msg, err := json.Marshal(result)
-			//if err != nil {
-			//	log.Logger.Error("json转换失败", err)
-			//	break
-			//}
-			//DataMsg := &sarama.ProducerMessage{}
-			//DataMsg.Topic = topic
-			//DataMsg.Partition = partition
-			//DataMsg.Value = sarama.StringEncoder(msg)
-			//_, _, err = kafkaProducer.SendMessage(DataMsg)
-			//if err != nil {
-			//	log.Logger.Error("向kafka发送消息失败", err)
-			//	break
-			//}
-			//
-			//log.Logger.Info(result.ReportId, " 报告消息发送结束")
+			// 发送结束消息
+			result := new(ResultDataMsg)
+			result.ReportId = reportId
+			result.End = true
+			result.MachineNum = num
+			msg, err := json.Marshal(result)
+			if err != nil {
+				log.Logger.Error("json转换失败", err)
+				break
+			}
+			DataMsg := &sarama.ProducerMessage{}
+			DataMsg.Topic = topic
+			DataMsg.Partition = partition
+			DataMsg.Value = sarama.StringEncoder(msg)
+			_, _, err = kafkaProducer.SendMessage(DataMsg)
+			if err != nil {
+				log.Logger.Error("向kafka发送消息失败", err)
+				break
+			}
+
+			log.Logger.Info(result.ReportId, " 报告消息发送结束")
 
 			return
 
